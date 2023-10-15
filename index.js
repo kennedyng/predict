42e22e80b030f7fe1dd592074c9562da38d99959
kennedyng <68716247+kennedyng@users.noreply.github.com>
2023-10-15 17:48:49 +0200

    new changes

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const express = require("express");
 var bodyParser = require("body-parser");
 const { NeuralNetwork, likely } = require("brain.js");
 const cors = require("cors");
-const trainData = require("./trainDataSet");
+const fs = require("fs");
 const app = express();
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors());
-
-const neuralNetwork = new NeuralNetwork();
 
 app.post("/predict", async (req, res) => {
   try {
-    neuralNetwork.train(trainData);
+    const neuralNetwork = new NeuralNetwork();
+
+    const networkState = JSON.parse(
+      fs.readFileSync("network_state.json", "utf-8")
+    );
+    neuralNetwork.fromJSON(networkState);
     const result = likely(req.body, neuralNetwork);
+
     res.status(201).json({ personality: result });
   } catch (error) {
     res.status(409);
--- trainNet.js
+++ trainNet.js
@@ -1,7 +1,45 @@
 const { NeuralNetwork, likely } = require("brain.js");
+const fs = require("fs");
 
-const trainData = require("./trainDataSet");
-const neuralNetwork = new NeuralNetwork();
+const data = require("./train.json");
 
-neuralNetwork.train(trainData);
-const personality = likely(req.body, neuralNetwork);
+const genderMap = {
+  Female: 1,
+  Male: 0,
+};
+
+const personalityMap = {
+  serious: { serious: 1 },
+  lively: { lively: 1 },
+  dependable: { dependable: 1 },
+  responsible: { responsible: 1 },
+  extraverted: { extraverted: 1 },
+};
+
+const trainingDataSet = data.map((record) => {
+  return {
+    input: {
+      gender: genderMap[record.Gender],
+      openness: record.openness,
+      age: record.Age,
+      neuroticism: record.neuroticism,
+      conscientiousness: record.conscientiousness,
+      agreeableness: record.agreeableness,
+      extraversion: record.extraversion,
+    },
+    output: personalityMap[record["Personality (Class label)"]],
+  };
+});
+
+console.log(trainingDataSet);
+
+const neuralNetwork = new NeuralNetwork({
+  hiddenLayers: [20, 20, 20],
+});
+
+neuralNetwork.train(trainingDataSet, {
+  log: (error) => console.log(error),
+});
+
+const networkState = neuralNetwork.toJSON();
+fs.writeFileSync("network_state.json", JSON.stringify(networkState), "utf-8");
